d984d2ae529d4728addb99faad58dfd2166315c0
Odiasesamuel <odiasesamuel4@gmail.com>
2022-06-30 02:58:19 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+##  a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line" ##
+## Use the function puts ##
 int main(void)
 
 {
